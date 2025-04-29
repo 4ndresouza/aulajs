767c1f9b92fb85d777150e0fffbf19b1559107d8
ANDRE AUGUSTO CESAR QUEIROZ DE SOUZA <andresouza6178@digitalcollege.internal>
2025-04-29 20:11:53 -0300

    aula01js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('testando o meu JS');
+
+document.title ='Calculadora'
+
+alert('Seu computador está infectado')
---
+++ tipos_dados.js
@@ -0,0 +1,42 @@
+//var,const e let
+
+//1-const (constante)
+//const pi = 3.14;
+
+// imprimindo a variável no console
+//console.log(pi);
+
+//pi = 8;
+//console.log(pi);
+
+
+//2 - let (o valor da variavel pode ser alterado)
+ let nome = 'angelo'
+console.log(nome)
+
+
+nome = 'Angelo Lindão!';
+console.log(nome)
+
+
+let desconto = 20.5
+//multiplicando a variavel sem alterar o original
+console.log(desconto * 2);
+
+
+//atulizando o valor da variavel utilizando a própria variável
+desconto = desconto * 2
+console.log('desconto atualizado' + desconto);
+
+
+//Array (lista)
+let tamanhos = ['p', 'pp','m','g' 36, 38, 40,48, 54];
+
+
+let temPet = false;
+
+const idade = 28;
+
+
+
+
