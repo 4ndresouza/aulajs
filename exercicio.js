c930d6bc9fb96d37b7a1bb42037a279040c89dd6
ANDRE AUGUSTO CESAR QUEIROZ DE SOUZA <andresouza6178@digitalcollege.internal>
2025-04-29 22:12:57 -0300

    funções

---
+++ exercicio.js
@@ -0,0 +1,43 @@
+//- Crie uma função que calcule a soma de 3 números exiba no console o resultado e á média
+
+
+//function somaDe3Numeros(n1, n2, n3){
+//const soma = n1 + n2 + n3;
+
+//const media = soma / 3;
+
+//return soma, media;
+}
+//console.log(somaDe3Numeros(234, 567, 890))
+
+//2 - Crie uma duas funções que calcule a soma e a média de 3 números exiba no console o resultado da soma e da média
+
+function soma(n1, n2, n3){
+const soma = n1 + n2 + n3;
+return soma;
+
+
+}
+
+
+
+function media (n1,n2,n3) {
+    //const soma = n1 + n2 + n3 /3;
+    const media = adição / 3;
+    const media = soma (n1,n2,n3)
+
+return "A soma é: " + adição + " e a média é:" + media;
+
+}
+
+//console.log(soma(234, 243, 342))
+//console,log(media(23, 23, 23))
+
+console.log(media(234, 234, 234))
+
+
+
+
+
+
+
